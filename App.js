7ee5d59dd32da9ad7f2928432b1b2fefe2f88300
Ian Harold <ianhunterharold@gmail.com>
2020-10-07 17:07:24 -0600

    first commit, playing around with expo and flexbox

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, ActivityIndicator,ProgressViewIOS, Button, Alert, Dimensions,Platform, ProgressBarAndroid, StyleSheet } from 'react-native';
 
-export default function App() {
+
+export default function App(){
+
+  const { height, width } = Dimensions.get('window');
+
+  const onButtonPress = () => {
+    Alert.alert(`${new Date().toLocaleTimeString()}clicked the button`)
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.page}>
+      <ActivityIndicator size='large' color='#61DBFB'/>
+      
+      <Text style={styles.text}>
+        piss off
+      </Text>
+      <Button title="Click Me" onPress={onButtonPress}></Button>
+      <Text style={styles.text}>Height: {height} and Width: {width} and device platform: {Platform.OS}</Text>
+      {Platform.OS === 'ios' && <ProgressViewIOS progress={0.5} />}
+      {Platform.OS ==='android' && (
+        <ProgressBarAndroid 
+          styleAttr='Horizontal'
+          indeterminate={false}
+          color='blue'
+        />
+      )} 
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  page: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'row',
+    
     alignItems: 'center',
-    justifyContent: 'center',
+
+    marginTop: 20,
+    backgroundColor: "#DDD",
+
   },
-});
+  text:{
+  
+    textAlign: 'center',
+    fontSize: 10,
+    color: "red",
+    backgroundColor: "yellow",
+    margin: 5,
+    padding: 5,
+  }
+
+})
